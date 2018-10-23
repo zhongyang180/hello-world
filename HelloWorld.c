d5a91d70a70b663553eb7b2f0f279edca972b6b6
钟阳 <zhongdamo@qq.com>
2018-10-23 20:52:07 +0800

    add Helll world

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 int main() {
-  printf("Hello World!\n");
+  //printf("Hello World!\n");
+  //system("pause"); 
+  int a,b,c;
+  a=1;b=2;c=a+b;
+  printf("%d",c);
+  
   return 0;
 }
